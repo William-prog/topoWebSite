8ef5f7ced759d93f0d2197ae1cdfb9c0c866856d
Alberto Ruiz Castro <topoiscalberto@topo-int.com>
2020-09-14 12:22:14 -0500

    auth and scss mixins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome back
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('hometopo');
 });
 
 Route::get('careers', function () {
     return view('careers');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
