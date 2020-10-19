954bb24263b0f68200e1266659c4906ccc527917
Alberto Ruiz Castro <topoiscalberto@topo-int.com>
2020-10-19 11:35:48 -0500

    partners slider up and running

--- resources/views/hometopo.blade.php
+++ resources/views/hometopo.blade.php
@@ -138,47 +138,41 @@
                                 <div class="slider">
 	<div class="slide-track">
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img src="img/partners/penoles-logo.png"  alt="peñoles" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img src="img/partners/minerafresnillo-logo.png" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img src="img/partners/robbins-logo.png"  alt="" />
+        </div>
+        <div class="slide">
+			<img src="img/partners/coeurmining-logo.png"  alt="" />
+        </div>        
+        <div class="slide">
+			<img src="img/partners/genco-logo.png" alt="" />
+        </div>
+        <div class="slide">
+			<img src="img/partners/micon-logo.png" width="195px" alt="" />
+        </div>
+        <div class="slide">
+			<img src="img/partners/penoles-logo.png" alt="peñoles" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img src="img/partners/minerafresnillo-logo.png"  alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img src="img/partners/robbins-logo.png" alt="" />
+        </div>
+        <div class="slide">
+			<img src="img/partners/coeurmining-logo.png"  alt="" />
+        </div>
+        <div class="slide">
+			<img src="img/partners/genco-logo.png"  alt="" />
 		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
-		</div>
+        <div class="slide">
+			<img src="img/partners/micon-logo.png" width="195px" alt="" />
+        </div>        
 	</div>
 </div>
                                 <!-- 
