ab84c703f6a64d7bd91a8daa9988c2896d1c8784
Alberto Ruiz Castro <topoiscalberto@topo-int.com>
2020-11-10 15:56:03 -0600

    made header into an include, fished layout issue in contact section

---
+++ resources/views/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <nav id="mobile-nav" class="flex flex-jc-sb flex-ai-c hide-desktop">
+        <a href="/">
+            <img id="mobile-logo" src="img/logo.png" alt="Servicios y Equipos Topo S.A. de C.V." class="logo">
+        </a>
+        <a href="#">
+            <img id="mobile-menu" class="burger-menu" src="img/menu-white.png" alt="menu" class="menu">
+        </a>
+    </nav>
+
+    <nav id="desktop-nav" class="col-4 flex-jc-sb flex-ai-c flex-jc-sa hide-mobile">
+        <div class="logo-desktop">
+            <a href="#">
+                <img id="desktop-logo" src="img/logo.png" alt="" class="">
+            </a>
+        </div>
+        <div>
+            <a class="nav-anchor" href="/">Inicio</a>
+        </div>
+        <div>
+            <a class="nav-anchor" href="#service-section">Servicios</a>
+        </div>            
+        <div>
+            <a class="nav-anchor" href="#contact-section">Contácto</a>
+        </div>
+        <div>
+            <a class="nav-anchor" href="careers">Empleos</a>
+        </div>
+    </nav>
+</header>
+
+<!-- Change header style when scrolling -->
+<script src="js/scroll.js"></script>
--- resources/views/hometopo.blade.php
+++ resources/views/hometopo.blade.php
@@ -23,36 +23,7 @@
     <title>Servicios y Equipos TOPO S.A. de C.V.</title>
 </head>
 <body>
-    <header>
-        <nav id="mobile-nav" class="flex flex-jc-sb flex-ai-c hide-desktop">
-            <a href="/">
-                <img id="mobile-logo" src="img/logo.png" alt="Servicios y Equipos Topo S.A. de C.V." class="logo">
-            </a>
-            <a href="#">
-                <img id="mobile-menu" class="burger-menu" src="img/menu-white.png" alt="menu" class="menu">
-            </a>
-        </nav>
-
-        <nav id="desktop-nav" class="col-4 flex-jc-sb flex-ai-c flex-jc-sa hide-mobile">
-            <div class="logo-desktop">
-                <a href="#">
-                    <img id="desktop-logo" src="img/logo.png" alt="" class="">
-                </a>
-            </div>
-            <div>
-                <a class="nav-anchor" href="/">Inicio</a>
-            </div>
-            <div>
-                <a class="nav-anchor" href="#service-section">Servicios</a>
-            </div>            
-            <div>
-                <a class="nav-anchor" href="#contact-section">Contácto</a>
-            </div>
-            <div>
-                <a class="nav-anchor" href="careers">Empleos</a>
-            </div>
-        </nav>
-    </header>
+    @include('header')
 
     <main>
         <section id="hero-section" class="hero-section">
@@ -314,8 +285,6 @@
     </footer>
 </body>
 </html>
-<!-- Change header style when scrolling -->
-<script src="js/scroll.js"></script>
 <!-- Jump to section when clicking anchor -->
 <script src="js/jump-section.js"></script>
 <!-- AOS fade animation library -->
