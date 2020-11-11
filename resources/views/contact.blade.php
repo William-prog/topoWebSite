743b9a7083ac1e33ca7dfaae0e901facee5e1bfc
Alberto Ruiz Castro <topoiscalberto@topo-int.com>
2020-11-11 11:41:43 -0600

    made changes to select input in Contact view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -27,8 +27,8 @@
                 <!-- Container for the maps cards -->
                 <div>
                     <!-- Mexico contact info -->
-                    <div class="gray-text white-bg container text-center extra-info-position shadow radius-20 pb-xxs">                                                
-                        <h2 class="pt-xs pb-xxs">Información adicional de contácto</h2>
+                    <div class="gray-text white-bg container text-center extra-info-position shadow radius-20 pb-xs">                                                
+                        <h2 class="pt-xs pb-xxs"><img src="img/mexicanflag.png" alt="¡Viva México!" width="10%"></h2>    
                         <div class="extra-info-column">
                             <div class="map-container radius-20">                                                  
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1834.1830784091885!2d-102.8918168!3d23.1568326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86830be391db3911%3A0xbe388955ea1b0ef6!2sServicios%20y%20Equipos%20Topo%20S.A.%20DE%20C.V.!5e0!3m2!1ses-419!2smx!4v1575931759306!5m2!1ses-419!2smx" 
@@ -58,32 +58,34 @@
                         </div>
                     </div>
                     <!-- USA contact info -->
-                    <div class="gray-text white-bg container text-center extra-info-position shadow radius-20 pb-xxs">                                                
-                        <h2 class="pt-xs pb-xs">Información adicional de contácto</h2>
-                        <div class="extra-info-column">
-                            <div class="map-container radius-20">                                                  
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1834.1830784091885!2d-102.8918168!3d23.1568326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86830be391db3911%3A0xbe388955ea1b0ef6!2sServicios%20y%20Equipos%20Topo%20S.A.%20DE%20C.V.!5e0!3m2!1ses-419!2smx!4v1575931759306!5m2!1ses-419!2smx" 
-                                    class="radius-20"
-                                    width="90%" 
-                                    height="100%"
-                                    frameborder="0" 
-                                    style="border:50px;" 
-                                    allowfullscreen="true">
-                                </iframe>
-                            </div>
-                            <div class="extra-contact">
-                                <h5></h5>
-                                <div class="contact-info-container">
-                                    <img src="img/phone.png" alt="teléfono servicios y equipos TOPO">
-                                    <h5>555 555 555 555</h5>
+                    <div class="pt-xs">
+                        <div class="gray-text white-bg container text-center extra-info-position shadow radius-20 pb-xxs">                                                
+                            <h2 class="pt-xs pb-xxs"><img src="img/usflag.png" alt="God bless America" width="10%"></h2>                            
+                            <div class="extra-info-column">
+                                <div class="map-container radius-20">                                                  
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1834.1830784091885!2d-102.8918168!3d23.1568326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86830be391db3911%3A0xbe388955ea1b0ef6!2sServicios%20y%20Equipos%20Topo%20S.A.%20DE%20C.V.!5e0!3m2!1ses-419!2smx!4v1575931759306!5m2!1ses-419!2smx" 
+                                        class="radius-20"
+                                        width="90%" 
+                                        height="100%"
+                                        frameborder="0" 
+                                        style="border:50px;" 
+                                        allowfullscreen="true">
+                                    </iframe>
                                 </div>
-                                <div class="contact-info-container">
-                                    <img src="img/mail.png" alt="teléfono servicios y equipos TOPO">
-                                    <h5>doe@topo-int.com</h5>
-                                </div>
-                                <div class="contact-info-container">
-                                    <img src="img/clock.png" alt="teléfono servicios y equipos TOPO">
-                                    <h5>Lun-Vie: 08:00am-06:00pm</h5>                        
+                                <div class="extra-contact">
+                                    <h5></h5>
+                                    <div class="contact-info-container">
+                                        <img src="img/phone.png" alt="teléfono servicios y equipos TOPO">
+                                        <h5>555 555 555 555</h5>
+                                    </div>
+                                    <div class="contact-info-container">
+                                        <img src="img/mail.png" alt="teléfono servicios y equipos TOPO">
+                                        <h5>doe@topo-int.com</h5>
+                                    </div>
+                                    <div class="contact-info-container">
+                                        <img src="img/clock.png" alt="teléfono servicios y equipos TOPO">
+                                        <h5>Lun-Vie: 08:00am-06:00pm</h5>                        
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -97,21 +99,27 @@
                             @csrf
                             <div class="form-grid-container">
                                 <div class="form-input-container">
-                                    <label for="full-name">Nombre completo</label>
-                                    <input required name="full-name" id="full-name" type="text" placeholder="Nombre completo">
+                                    <label for="full-name">Nombre completo:</label>
+                                    <input required name="full-name" id="full-name" type="text">
                                 </div>
                                 <div class="form-input-container">
-                                    <label for="e-mail">Correo electrónico</label>
-                                    <input required name="sender" id="e-mail" type="text" placeholder="Correo electrónico" class="mt-xs">
+                                    <label for="e-mail">Correo electrónico:</label>
+                                    <input required name="sender" id="e-mail" type="text" class="mt-xs">
                                 </div>
                                 <div class="form-input-container">
-                                    <label for="subject">Asunto del mensaje</label>
-                                    <input required name="subject" id="subject" type="text" placeholder="Asunto" class="mt-xs">
-                                </div>
-                                <div class="form-input-container">
-                                    <label for="department">Con destino a</label>
+                                    <label for="subject">Asunto del mensaje:</label>
                                     <select required name="to-contact" id="department">
-                                        <option value="" disabled selected>Departamento al que dirige</option>
+                                        <option value="" disabled selected>Elige una opción</option>
+                                        <option value="Solicito información">Solicito información</option>
+                                        <option value="Sugerencia">Sugerencia</option>
+                                        <option value="Soy proveedor de servicios y/o productos">Soy proveedor de servicios y/o productos</option>
+                                        <option value="Cotización">Cotización</option>
+                                    </select>
+                                </div>
+                                <div class="form-input-container">
+                                    <label for="department">Dirigido a:</label>
+                                    <select required name="to-contact" id="department">
+                                        <option value="" disabled selected>Elige una opción</option>
                                         <option value="Recursos humanos">Recursos humanos</option>
                                         <option value="Servicio a clientes">Servicio a clientes</option>
                                         <option value="Compras">Compras</option>
@@ -119,7 +127,7 @@
                                 </div>
                             </div>
                             <div class="form-input-container">
-                                <label for="message">Mensaje</label>
+                                <label for="message">Mensaje:</label>
                                 <textarea required name="message" id="message" cols="30" rows="4"></textarea>
                             </div>
                             <div class="form-button-container">                                        
