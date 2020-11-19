4ea25e84f2e4c23a6bf0eac65d606c5a11b17f16
Alberto Ruiz Castro <topoiscalberto@topo-int.com>
2020-11-19 13:26:16 -0600

    working on the careers view

--- resources/views/careers.blade.php
+++ resources/views/careers.blade.php
@@ -1,77 +1,48 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#FF781F" />
+    <meta name="description" content="Servicios y equipos de mineria con más de 30 años de experiencia. Reparación y mantenimiento, contamos con cabina de pintura, shotblast, mano de obra minera.">
+    <meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
+    <!-- Link tags -->
+    <link rel="icon" type="image/png" href="img/favicon.png"/>
+    <link rel="stylesheet" href="css/appCustomStyles.css">
+    <title>Empleos - Servicios y Equipos Topo S.A. de C.V.</title>
+</head>
 
-        <title>Laravel</title>
+<body>
+    @include('header')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-   
-            <div class="content">
-                <div class="title m-b-md">
-                    Page under construction
+    <main>
+        <div class="wrapper">
+            <section class="careers-section">
+                <div class="careers-img-container">
+                    <img src="img/team.JPG" alt="Empleados de Servicios y Equipos Topo S.A. de C.V.">
+                    <div class="career-img-overlay">
+                        <h1>Forma parte del equipo <span class="orange-text">Topo</span></h1>
+                    </div>
                 </div>
 
-            </div>
+                <div class="company-values">
+                    <h2 class="text-center">¿Qué nos hace diferentes?</h2>
+                    <div class="values-container text-center">
+                        <h3>Value name</h3>
+                        <span class="orange-text"><i class="fas fa-bullhorn"></i></span>
+                    </div>
+                </div>
+            </section>
         </div>
-    </body>
+    </main>
+
+    @include('footer')
+</body>
 </html>
+<!-- Change header style when scrolling -->
+<script src="js/scroll.js"></script>
+<!-- Font awesome CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
